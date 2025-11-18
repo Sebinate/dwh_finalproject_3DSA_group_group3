912082be9739668313cccf7b5be56926b3a5faca
kenzodebola <kenzolampa@gmail.com>
2025-11-18 17:17:54 +0800

    added column changers

--- scripts/utils/transform_utils.py
+++ scripts/utils/transform_utils.py
@@ -28,3 +28,27 @@ def columndropinator(df: pd.DataFrame) -> pd.DataFrame:
     else:
         print("'Unnamed: 0' is not present here")
     return df
+
+def stringinator(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    if column in df.columns:
+        df[column] = df[column].astype(str)
+    else:
+        print(f"Column '{column}' does not exist in the DataFrame.")
+    return df
+
+def floatinator(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    if column in df.columns:
+        df[column] = df[column].astype(float)
+    else:
+        print(f"Column '{column}' does not exist in the DataFrame.")
+    return df
+
+def datetimeinator(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    if column in df.columns:
+        try:
+            df[column] = pd.to_datetime(df[column], errors='coerce')
+        except Exception as e:
+            print(f"Error converting column '{column}' to datetime: {e}")
+    else:
+        print(f"Column '{column}' does not exist in the DataFrame.")
+    return df

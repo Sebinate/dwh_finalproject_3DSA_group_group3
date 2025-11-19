95690a909d6e7fb59ac5f09d7c3303f7ceca0c3c
Sebinate <sebastiansampao@gmail.com>
2025-11-19 22:18:12 +0800

    Modularizing Ingestion

--- infra/ingestion/business_dept/product_list_ingest.py
+++ infra/ingestion/business_dept/product_list_ingest.py
@@ -34,43 +34,60 @@ file_match_path = os.path.join(PATH, pattern)
 #Connecting to db
 engine = utils.connect()
 
-file_list = glob.glob(file_match_path, recursive = True)
+file_paths = glob.glob(file_match_path, recursive = True)
 
-if not file_list:
-    print("No new files found")
+cleaners = [(transform_utils.columndropinator),
+            (column_renaminator),
+            (nullinator),
+            (transform_utils.unduplicateinator, "product_id"),
+            (transform_utils.stringinator, "product_id"),
+            (transform_utils.stringinator, "product_name"),
+            (transform_utils.stringinator, "product_type"),
+            (transform_utils.floatinator, "product_price"),
+            ]
 
-else:
-    staging_table_name = pattern.split("*")[0]
+product_ingester = ingest_utils.Ingest(engine = engine, 
+                               cleaners = cleaners, 
+                               file_paths = file_paths, 
+                               pattern = pattern)
 
-    inspector = inspect(engine)
+product_ingester.ingest()
 
-    for file_path in file_list:
-        file_type = file_path.split(r"\\")[-1].split(".")[-1]
+# if not file_list:
+#     print("No new files found")
 
-        reader = ingest_utils.file_type_reader(file_type)
+# else:
+#     staging_table_name = pattern.split("*")[0]
 
-        if file_type == "csv" or file_type == "parquet":
-            for batch in reader(file_path):
-                batch = transform_utils.columndropinator(batch)
-                batch = column_renaminator(batch)
-                batch = nullinator(batch)
-                batch = transform_utils.unduplicateinator(batch, "product_id")  
-                batch = transform_utils.stringinator(batch, "product_id")
-                batch = transform_utils.stringinator(batch, "product_name")
-                batch = transform_utils.stringinator(batch, "product_type")
-                batch = transform_utils.floatinator(batch, "product_price")
-                batch = transform_utils.unduplicateinator(batch, "product_id")  
-                batch.to_sql(name = staging_table_name, con = engine, if_exists = "append")
+#     inspector = inspect(engine)
 
-        else:
-            data = reader(file_path)
-            data = transform_utils.columndropinator(data)
-            data = column_renaminator(data)
-            data = nullinator(data)
-            data = transform_utils.unduplicateinator(data, "product_id")  
-            data = transform_utils.stringinator(data, "product_id")
-            data = transform_utils.stringinator(data, "product_name")
-            data = transform_utils.stringinator(data, "product_type")
-            data = transform_utils.floatinator(data, "product_price")
-            data = transform_utils.unduplicateinator(data, "product_id")
-            data.to_sql(name = staging_table_name, con = engine, if_exists = "append")
+#     for file_path in file_list:
+#         file_type = file_path.split(r"\\")[-1].split(".")[-1]
+
+#         reader = ingest_utils.file_type_reader(file_type)
+
+#         if file_type == "csv" or file_type == "parquet":
+#             for batch in reader(file_path):
+#                 batch = transform_utils.columndropinator(batch)
+#                 batch = column_renaminator(batch)
+#                 batch = nullinator(batch)
+#                 batch = transform_utils.unduplicateinator(batch, "product_id")  
+#                 batch = transform_utils.stringinator(batch, "product_id")
+#                 batch = transform_utils.stringinator(batch, "product_name")
+#                 batch = transform_utils.stringinator(batch, "product_type")
+#                 batch = transform_utils.floatinator(batch, "product_price")
+#                 batch = transform_utils.unduplicateinator(batch, "product_id")  
+#                 batch.to_sql(name = staging_table_name, con = engine, if_exists = "append")
+
+#         else:
+#             data = reader(file_path)
+#             data = transform_utils.columndropinator(data)
+#             data = column_renaminator(data)
+#             data = nullinator(data)
+#             data = transform_utils.unduplicateinator(data, "product_id")  
+#             data = transform_utils.stringinator(data, "product_id")
+#             data = transform_utils.stringinator(data, "product_name")
+#             data = transform_utils.stringinator(data, "product_type")
+#             data = transform_utils.floatinator(data, "product_price")
+#             data = transform_utils.unduplicateinator(data, "product_id")
+#             data.to_sql(name = staging_table_name, con = engine, if_exists = "append")
--- scripts/utils/ingest_utils.py
+++ scripts/utils/ingest_utils.py
@@ -1,9 +1,11 @@
 import pandas as pd
-from typing import Iterator
 import pickle as pkl
-from bs4 import BeautifulSoup
 import pyarrow.parquet as pq
+
+from typing import Iterator
+from bs4 import BeautifulSoup
 from sqlalchemy import create_engine, Engine, inspect
+from scripts.utils import transform_utils
 
 def csv_reader(path: str, chunksize: int = 10_000) -> Iterator[pd.DataFrame]:
     data = pd.read_csv(path, chunksize = chunksize, sep = None, index_col = False)
@@ -65,6 +67,67 @@ def file_type_reader(file_type: str):
     
     elif file_type == "json":
         return json_reader
+    
+def column_renaminator(df: pd.DataFrame) -> pd.DataFrame:
+    renames = {
+        'price': 'product_price',
+    }
+
+    df = df.rename(columns=renames)
+    print(df.columns)
+
+    return df
+
+def nullinator(df: pd.DataFrame) -> pd.DataFrame:
+    fill_rules = {
+        "product_type": "Uncategorized",
+    }
+
+    df = df.fillna(value=fill_rules)
+
+    return df
+
+class Ingest:
+    def __init__(self, 
+                 engine: Engine,
+                 cleaners: list[tuple],
+                 file_paths: list,
+                 pattern: str):
+        self.engine = engine
+        self.cleaners = cleaners
+        self.file_paths = file_paths
+        self.pattern = pattern
+    
+    def ingest(self):
+        if not self.file_paths:
+            print("No new files found")
+            return
+
+        staging_table_name = self.pattern.split("*")[0]
+
+        for file_path in self.file_paths:
+            file_type = file_path.split(r"\\")[-1].split(".")[-1]
+
+            reader = file_type_reader(file_type)
+
+            if file_type == "csv" or file_type == "parquet":
+                for batch in reader(file_path):
+                    for cleaner in self.cleaners:
+                        if len(cleaner) > 1:
+                            batch = cleaner[0](batch, *cleaner[1:])
+                        else:
+                            batch = cleaner[0](batch)
+                    batch.to_sql(name = staging_table_name, con = self.engine, if_exists = "append")
+
+            else:
+                data = reader(file_path)
+                for cleaner in self.cleaners:
+                    if len(cleaner) > 1:
+                        data = cleaner[0](data, *cleaner[1:])
+                    else:
+                        data = cleaner[0](data)
+                    data = cleaner[0](data, *cleaner[:-1])
+                data.to_sql(name = staging_table_name, con = self.engine, if_exists = "append")
 
 if __name__ == "__main__":
     print(html_reader(r"C:\Users\User\dwh_finalproject_3DSA_group_group3\data\Project Dataset-20241024T131910Z-001\Operations Department\order_delays.html"))

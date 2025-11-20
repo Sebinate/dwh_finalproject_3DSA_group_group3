ddbde7f3bad905ee43c41b79d65bb30d90ec7a2a
kenzodebola <kenzolampa@gmail.com>
2025-11-20 22:53:32 +0800

    Removed undeleted functions

--- infra/ingestion/operations_dept/order_data_ingest.py
+++ infra/ingestion/operations_dept/order_data_ingest.py
@@ -6,16 +6,6 @@ from scripts.utils import ingest_utils
 from scripts.utils import utils
 from sqlalchemy import inspect
 
-def column_renaminator(df: pd.DataFrame) -> pd.DataFrame:
-    renames = {
-        'estimated arrival': 'transact_estimated_arrival_days',
-        'transaction_date': 'transact_date',
-    }
-
-    df = df.rename(columns=renames)
-
-    return df
-
 # Make this dynamic in the future
 PATH = r"data/Project Dataset-20241024T131910Z-001/Operations Department"
 pattern = r"order_data*"

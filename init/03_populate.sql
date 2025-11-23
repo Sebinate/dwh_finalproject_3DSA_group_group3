52c1e8e12c2f829222d75948bd7a6ddd43f3d9a5
Chaurell Untalan <phasechau@gmail.com>
2025-11-23 18:07:41 +0800

    changed range of date creation from 1970 - 2030 to account for user birthdate and 2020  3030 for transacs

--- init/03_populate.sql
+++ init/03_populate.sql
@@ -37,7 +37,7 @@ SELECT
 FROM (
     -- Generates 10 years of dates. Adjust start/end as needed.
     SELECT generate_series(
-        '1970-01-01'::DATE, 
+        '2020-01-01'::DATE, 
         '2030-12-31'::DATE, 
         '1 day'::INTERVAL
     ) AS datum
@@ -50,7 +50,48 @@ SELECT * FROM dim_date
 ON CONFLICT (date_key) DO NOTHING;
 
 INSERT INTO dim_user_date
-SELECT * FROM dim_date
+SELECT
+    -- 1. date_key (YYYYMMDD)
+    TO_CHAR(datum, 'yyyymmdd')::INT,
+
+    -- 2. date_full
+    datum,
+
+    -- 3. date_day_of_week (Postgres ISODOW: Monday=1, Sunday=7)
+    EXTRACT(ISODOW FROM datum),
+
+    -- 4. date_month_num
+    EXTRACT(MONTH FROM datum),
+
+    -- 5. date_day_name
+    TO_CHAR(datum, 'Day'),
+
+    -- 6. date_day_of_month
+    EXTRACT(DAY FROM datum),
+
+    -- 7. date_day_of_year
+    EXTRACT(DOY FROM datum),
+
+    -- 8. date_week_of_year
+    EXTRACT(WEEK FROM datum),
+
+    -- 9. date_month_name
+    TO_CHAR(datum, 'Month'),
+
+    -- 10. date_quarter
+    EXTRACT(QUARTER FROM datum),
+
+    -- 11. date_year
+    EXTRACT(YEAR FROM datum)
+
+FROM (
+    -- Generates 10 years of dates. Adjust start/end as needed.
+    SELECT generate_series(
+        '1970-01-01'::DATE, 
+        '2030-12-31'::DATE, 
+        '1 day'::INTERVAL
+    ) AS datum
+) date_sequence
 ON CONFLICT (date_key) DO NOTHING;
 
 INSERT INTO dim_staff_date

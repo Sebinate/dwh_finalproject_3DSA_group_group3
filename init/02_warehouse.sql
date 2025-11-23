8bcb73d72fdae2eea9f70bfd42ff990511c4e412
Chaurell Untalan <phasechau@gmail.com>
2025-11-23 13:54:20 +0800

    warehouse draft complete, waiting for further instructions

--- init/02_warehouse.sql
+++ init/02_warehouse.sql
@@ -1,4 +1,6 @@
 -- Insert CREATE TABLE table IF NOT EXISTS here, date outriggers first then dims, facts last
+
+-- Outriggers
 CREATE TABLE IF NOT EXISTS dim_order_date (
     date_key             INT PRIMARY KEY,        -- 20251123 -
     date_full            DATE NOT NULL,          -- 2025-11-23 -
@@ -12,4 +14,153 @@ CREATE TABLE IF NOT EXISTS dim_order_date (
     date_month_name      VARCHAR(10) NOT NULL,   -- 'November' -
     date_quarter         INT NOT NULL,           -- 4 -
     date_year            INT NOT NULL            -- 2025 -
+);
+
+CREATE TABLE IF NOT EXISTS dim_user_date (
+    date_key              INT PRIMARY KEY,
+    date_full             DATE NOT NULL,
+    date_day_of_week      INT NOT NULL,
+    date_month_num        INT NOT NULL,
+    date_day_name         VARCHAR(10) NOT NULL,
+    date_day_of_month     INT NOT NULL,
+    date_day_of_year      INT NOT NULL,
+    date_week_of_year     INT NOT NULL,
+    date_month_name       VARCHAR(10) NOT NULL,
+    date_quarter          INT NOT NULL,
+    date_year             INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dim_merchant_date (
+    date_key              INT PRIMARY KEY,
+    date_full             DATE NOT NULL,
+    date_day_of_week      INT NOT NULL,
+    date_month_num        INT NOT NULL,
+    date_day_name         VARCHAR(10) NOT NULL,
+    date_day_of_month     INT NOT NULL,
+    date_day_of_year      INT NOT NULL,
+    date_week_of_year     INT NOT NULL,
+    date_month_name       VARCHAR(10) NOT NULL,
+    date_quarter          INT NOT NULL,
+    date_year             INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dim_staff_date (
+    date_key              INT PRIMARY KEY,
+    date_full             DATE NOT NULL,
+    date_day_of_week      INT NOT NULL,
+    date_month_num        INT NOT NULL,
+    date_day_name         VARCHAR(10) NOT NULL,
+    date_day_of_month     INT NOT NULL,
+    date_day_of_year      INT NOT NULL,
+    date_week_of_year     INT NOT NULL,
+    date_month_name       VARCHAR(10) NOT NULL,
+    date_quarter          INT NOT NULL,
+    date_year             INT NOT NULL
+);
+
+-- Dimensions
+CREATE TABLE IF NOT EXISTS dim_date (
+    date_key              INT PRIMARY KEY,
+    date_full             DATE NOT NULL,
+    date_day_of_week      INT NOT NULL,
+    date_month_num        INT NOT NULL,
+    date_day_name         VARCHAR(10) NOT NULL,
+    date_day_of_month     INT NOT NULL,
+    date_day_of_year      INT NOT NULL,
+    date_week_of_year     INT NOT NULL,
+    date_month_name       VARCHAR(10) NOT NULL,
+    date_quarter          INT NOT NULL,
+    date_year             INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dim_product_list (
+    product_key           INT PRIMARY KEY,        
+    product_id            INT NOT NULL,
+    product_name          VARCHAR(255),
+    product_type          VARCHAR(50),
+    product_price         NUMERIC(10, 2)
+);
+
+CREATE TABLE IF NOT EXISTS dim_campaign (
+    campaign_key          INT PRIMARY KEY,        
+    campaign_id           INT NOT NULL,
+    campaign_name         VARCHAR(255),
+    campaign_desc         VARCHAR(500),
+    campaign_discount     NUMERIC(5, 2)
+);
+
+CREATE TABLE IF NOT EXISTS dim_staff (
+    staff_key             INT PRIMARY KEY,        
+    staff_id              INT NOT NULL,
+    staff_name            VARCHAR(255),
+    staff_job_level       VARCHAR(50),
+    staff_street          VARCHAR(255),
+    staff_state           VARCHAR(50),
+    staff_city            VARCHAR(50),
+    staff_country         VARCHAR(50),
+    staff_contact_number  VARCHAR(20),
+    staff_creation_date   DATE,
+    staff_creation_date_key INT REFERENCES dim_staff_date(date_key)
+);
+
+
+CREATE TABLE IF NOT EXISTS dim_user (
+    user_key              INT PRIMARY KEY,        
+    user_id               INT NOT NULL,
+    user_name             VARCHAR(255),
+    user_ccn              VARCHAR(50),           
+    user_bank             VARCHAR(100),
+    user_street           VARCHAR(255),
+    user_city             VARCHAR(50),
+    user_country          VARCHAR(50),
+    user_birthdate        DATE,
+    user_gender           VARCHAR(10),
+    user_device_type      VARCHAR(50),
+    user_type             VARCHAR(50),
+    user_job_title        VARCHAR(100),
+    user_job_level        VARCHAR(50),
+    user_creation_date_key INT REFERENCES dim_user_date(date_key)
+    user_birthdate_key     INT REFERENCES dim_date(date_key) 
+);
+
+CREATE TABLE IF NOT EXISTS dim_merchant (
+    merchant_key          INT PRIMARY KEY,        
+    merchant_id           INT NOT NULL,
+    merchant_creation_date DATE,
+    merchant_name         VARCHAR(255),
+    merchant_street       VARCHAR(255),
+    merchant_state        VARCHAR(50),
+    merchant_city         VARCHAR(50),
+    merchant_country      VARCHAR(50),
+    merchant_contact      VARCHAR(20),
+    merchant_creation_date_key INT REFERENCES dim_merchant_date(date_key)
+);
+
+CREATE TABLE IF NOT EXISTS dim_table (
+    order_key                 INT PRIMARY KEY,    
+    order_id                  INT NOT NULL,       
+    order_transac_date        DATE NOT NULL,
+    order_estimated_arrival   DATE,
+)
+
+-- Fact Tables
+CREATE TABLE IF NOT EXISTS fact_promotion_eligibility (
+    campaign_key          INT REFERENCES dim_campaign(campaign_key),
+    order_key             INT REFERENCES dim_order(order_key),
+    PRIMARY KEY (campaign_key, order_key)
+);
+
+CREATE TABLE IF NOT EXISTS fact_transaction (
+    date_key              INT REFERENCES dim_date(date_key),
+    staff_key             INT REFERENCES dim_staff(staff_key),
+    merchant_key          INT REFERENCES dim_merchant(merchant_key),
+    user_key              INT REFERENCES dim_user(user_key),
+    product_key           INT REFERENCES dim_product(product_key),
+    order_key             INT REFERENCES dim_order(order_key),
+    -- Measures
+    delay                 INT,                    
+    price                 NUMERIC(10, 2),
+    quantity              INT,
+
+    PRIMARY KEY (date_key, staff_key, merchant_key, user_key, product_key, order_key)
 );

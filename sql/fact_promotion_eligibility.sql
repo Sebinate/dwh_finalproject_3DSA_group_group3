d06264433b09c097c1c37a8b67774a527c7fc7f2
kenzodebola <kenzolampa@gmail.com>
2025-11-23 16:19:38 +0800

    SQL insertions

--- sql/fact_promotion_eligibility.sql
+++ sql/fact_promotion_eligibility.sql
@@ -1,4 +1,4 @@
-    INSERT INTO Fact_Promotion_Eligibility (
+    INSERT INTO warehouse.Fact_Promotion_Eligibility (
         CampaignKey,
         OrderKey
     )
@@ -6,7 +6,7 @@
         dim_campaign.CampaignKey,
         dim_order.OrderKey
         FROM staging.promotion_eligibility
-    JOIN dim_campaign
+    LEFT JOIN warehouse.dim_campaign
         ON staging.promotion_eligibility.campaign_id = dim_campaign.campaign_id
-    JOIN dim_order
+    LEFT JOIN warehouse.dim_order
         ON staging.promotion_eligibility.order_id = dim_order.order_id;

98625f8e3791ac83be10b56f87697dbd39954385
Sebinate <sebastiansampao@gmail.com>
2025-12-10 23:36:17 +0800

    Created new view for dashboard

--- sql/view2.sql
+++ sql/view2.sql
@@ -1,10 +1,13 @@
-CREATE OR REPLACE VIEW analytics.view2 AS 
+CREATE OR REPLACE VIEW analytics.campaign_data AS 
 SELECT
-    campaign_name, campaign_desc, campaign_discount, order_id, dod.date_full AS "date"
+    campaign_name,
+    SUM((quantity * (price - (price * campaign_discount)))) AS revenue,
+    COUNT(order_id) AS volume
 FROM warehouse.fact_promotion_eligibility AS fpe
 JOIN warehouse.dim_campaign AS campaign
     ON campaign.campaign_key = fpe.campaign_key
 JOIN warehouse.dim_order AS orders
     ON orders.order_key = fpe.order_key
-JOIN warehouse.dim_order_date AS dod
-    ON dod.date_key = orders.order_transac_date_key;
+JOIN warehouse.fact_transaction AS transactions
+    ON transactions.order_key = orders.order_key
+GROUP BY campaign_name;
